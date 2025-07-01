21fd3a279c9cb90a4dc41850de3ca68c2205d219
Asfm445 <awelabubekar@gmail.com>
2025-06-30 22:39:28 -0700

    about media

--- backend/backend2/urls.py
+++ backend/backend2/urls.py
@@ -26,13 +26,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path("api/docs/schema/ui/", SpectacularSwaggerView.as_view()),
     path("api/", include("api.urls")),
     path("api/user/register/", createUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
